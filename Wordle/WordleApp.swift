a2b88fdd4613ca777c52c674d2a7d477d7bf8ae5
Artem Paliutin <pal.artem@icloud.com>
2022-03-07 12:31:19 +0300

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Artem Paliutin on 07/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
